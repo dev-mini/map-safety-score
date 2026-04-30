5ae07eb36a66d1987bcb5f4b45c2abccc1265194
devMini <edgar.danmm@gmail.com>
2026-04-30 17:28:46 -0600

    Add(auth): add auth0 provider and initial config

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN;
+const CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const SUCCESS_URL = import.meta.env.VITE_AUTH0_SUCCESS_URL;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain={DOMAIN}
+      clientId={CLIENT_ID}
+      authorizationParams={{ redirect_uri: SUCCESS_URL }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>
 );
