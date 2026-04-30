c29b06eeaa71e2d52cf80ff39d154921bfbb834a
devMini <edgar.danmm@gmail.com>
2026-04-30 17:26:38 -0600

    Add(layout): add main layout component

---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router';
+import Header from './Header';
+
+const MainLayout = () => {
+  return (
+    <div className="dark flex flex-col w-full h-dvh bg-secondary text-foreground">
+      <Header />
+      <div className="p-4">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
