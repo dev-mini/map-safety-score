fb1d4d201240adc2b4588ace23fdbfd404c7a595
devMini <edgar.danmm@gmail.com>
2026-05-03 17:00:02 -0600

    Update(home): pass background image to hero component and update responsive design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,16 +5,23 @@ const Hero = () => {
   const navigate = useNavigate();
 
   return (
-    <section className="flex flex-col max-w-lg items-center justify-center h-84">
-      <h1 className="text-5xl xl:text-7xl font-bold bg-linear-to-r from-primary via-purple-300 to-foreground bg-clip-text text-transparent text-center my-10 ">
-        Map Safety Score
-      </h1>
-      <Button
-        className="my-4 w-32 xl:text-lg"
-        onClick={() => navigate('map-overview')}
-      >
-        View Map
-      </Button>
+    <section className="relative">
+      <img
+        src="/src/assets/images/bg-map.png"
+        alt="Map background"
+        className="flex absolute top-0 left-0 w-full h-84 object-right object-cover rounded-xl md:object-top brightness-50 md:brightness-90 -z-10 mask-[linear-gradient(to_bottom,black,transparent)]"
+      />
+      <div className="flex flex-col items-center justify-center w-full md:max-w-lg h-84">
+        <h1 className="text-5xl xl:text-7xl font-bold bg-linear-to-r from-primary via-purple-300 to-foreground bg-clip-text text-transparent text-center my-10">
+          Map Safety Score
+        </h1>
+        <Button
+          className="my-4 w-32 xl:text-lg"
+          onClick={() => navigate('map-overview')}
+        >
+          View Map
+        </Button>
+      </div>
     </section>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,9 @@ import { MapPinned, ShieldAlert, Speech } from 'lucide-react';
 
 const Home = () => {
   return (
-    <div className="flex flex-col relative h-full w-full">
-      <img
-        src="/src/assets/images/bg-map.png"
-        alt="Map background"
-        className="flex absolute top-0 left-0 w-full h-84 object-right object-cover rounded-xl md:object-top brightness-50 md:brightness-90 -z-10 mask-[linear-gradient(to_bottom,black,transparent)]"
-      />
+    <div className="flex flex-col w-full">
       <Hero />
-      <section className="flex flex-col md:flex-row mt-28 gap-8">
+      <section className="flex flex-col md:flex-row my-8 md:mt-20 gap-8">
         <article className="flex flex-col md:flex-1/3 gap-2 px-4 rounded-lg py-4 bg-card w-full">
           <MapPinned className="size-8 text-muted-foreground" />
           <h3 className="font-bold text-base xl:text-xl text-primary">
