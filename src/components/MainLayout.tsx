cb4679050ec48587f0306fd4fe226f8cfb5d9e26
devMini <edgar.danmm@gmail.com>
2026-05-02 23:46:47 -0600

    Update(auth): handle error toast inside useEffect

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -3,12 +3,15 @@ import Header from './Header';
 import LoadingScreen from './LoadingScreen';
 import { useAuth0 } from '@auth0/auth0-react';
 import { toast } from 'sonner';
+import { useEffect } from 'react';
 
 const MainLayout = () => {
   const { isLoading, error } = useAuth0();
 
-  if (error)
-    toast.error(error?.message ?? 'Ocurrió un error al iniciar sesión');
+  useEffect(() => {
+    if (error)
+      toast.error(error?.message ?? 'Ocurrió un error al iniciar sesión');
+  }, [error]);
 
   return (
     <div className="flex flex-col w-full h-dvh bg-background text-foreground">
