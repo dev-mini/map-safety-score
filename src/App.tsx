edbaff05c66b10a2d335230b19834f89f43d47ef
devMini <edgar.danmm@gmail.com>
2026-05-03 16:25:47 -0600

    Add(map-overview): add route and initial component for map overview feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import './App.css';
 import MainLayout from './components/MainLayout';
 import Home from './pages/Home';
+import MapOverview from './pages/MapOverview';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} index />
+          <Route path="map-overview" element={<MapOverview />} />
         </Route>
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
---
+++ src/pages/MapOverview.tsx
@@ -0,0 +1,9 @@
+const MapOverview = () => {
+  return (
+    <div>
+      <h1>Map Overview</h1>
+    </div>
+  );
+};
+
+export default MapOverview;
