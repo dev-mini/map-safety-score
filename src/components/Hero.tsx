aeb14525edb5af8bf3cbc6e4689d03a58d2bc778
devMini <edgar.danmm@gmail.com>
2026-05-03 16:22:10 -0600

    Update(home): update hero text color

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { Button } from './ui/button';
 const Hero = () => {
   return (
     <section className="flex flex-col max-w-lg items-center justify-center h-84">
-      <h1 className="text-5xl xl:text-7xl font-bold bg-linear-to-r from-primary via-purple-400 to-foreground bg-clip-text text-transparent text-center my-10 ">
+      <h1 className="text-5xl xl:text-7xl font-bold bg-linear-to-r from-primary via-purple-300 to-foreground bg-clip-text text-transparent text-center my-10 ">
         Map Safety Score
       </h1>
       <Button className="my-4 w-32 xl:text-lg">View Map</Button>
