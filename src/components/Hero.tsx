5bc95d7a74413359a3f795c96fc71265e51e17d7
devMini <edgar.danmm@gmail.com>
2026-05-03 16:26:08 -0600

    Add(map-overview): handle navigation from hero to map overview

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,20 @@
 import { Button } from './ui/button';
+import { useNavigate } from 'react-router';
 
 const Hero = () => {
+  const navigate = useNavigate();
+
   return (
     <section className="flex flex-col max-w-lg items-center justify-center h-84">
       <h1 className="text-5xl xl:text-7xl font-bold bg-linear-to-r from-primary via-purple-300 to-foreground bg-clip-text text-transparent text-center my-10 ">
         Map Safety Score
       </h1>
-      <Button className="my-4 w-32 xl:text-lg">View Map</Button>
+      <Button
+        className="my-4 w-32 xl:text-lg"
+        onClick={() => navigate('map-overview')}
+      >
+        View Map
+      </Button>
     </section>
   );
 };
