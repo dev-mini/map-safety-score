200b04dec64c7c07e5db70bd003346493be84d87
devMini <edgar.danmm@gmail.com>
2026-05-02 23:41:17 -0600

    Update(auth): handle login, signup and logout on header component and display user avatar. Also handle error message and loading state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,100 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from './ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import {
+  CreditCardIcon,
+  LogOutIcon,
+  SettingsIcon,
+  UserIcon,
+} from 'lucide-react';
 
 const Header = () => {
+  const {
+    user,
+    isAuthenticated,
+    loginWithRedirect: login,
+    logout: auth0Logout,
+    isLoading,
+  } = useAuth0();
+
+  const signUp = () =>
+    login({ authorizationParams: { screen_hint: 'signup' } });
+
+  const logout = () =>
+    auth0Logout({ logoutParams: { returnTo: window.location.origin } });
+
   return (
     <div className="flex items-center justify-between border-b border-foreground px-4 py-1">
-      <div className="flex gap-2 items-center ml-auto">
-        <Button
-          size={'sm'}
-          className="bg-foreground text-background hover:text-foreground"
-        >
-          Sign Up
-        </Button>
-        <Button
-          size={'sm'}
-          className="bg-foreground text-background hover:text-foreground"
-        >
-          Sign In
-        </Button>
-      </div>
+      {isAuthenticated && user ? (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild className="ml-auto">
+            <Button
+              variant="ghost"
+              size="icon"
+              className="rounded-full focus-visible:ring-transparent focus-visible:border-none focus-visible:border-0 focus-visible:ring-0 focus-visible:border-transparent"
+            >
+              <Avatar>
+                <AvatarImage
+                  decoding="async"
+                  src="https://github.com/shadcn.png"
+                  alt={user?.name}
+                />
+                <AvatarFallback>
+                  {user?.name?.charAt(0).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-32 bg-background z-50">
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="cursor-pointer hover:bg-foreground/20 text-sm rounded-md">
+                <UserIcon className="size-3.5" />
+                Profile
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer hover:bg-foreground/20 text-sm rounded-md">
+                <CreditCardIcon className="size-3.5" />
+                Billing
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer hover:bg-foreground/20 text-sm rounded-md">
+                <SettingsIcon className="size-3.5" />
+                Settings
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem variant="destructive" onClick={logout}>
+              <LogOutIcon className="size-3.5" />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ) : (
+        <div className="flex gap-2 items-center ml-auto">
+          <Button
+            size={'sm'}
+            className="bg-foreground text-background hover:text-foreground"
+            onClick={signUp}
+            disabled={isLoading}
+          >
+            Sign Up
+          </Button>
+          <Button
+            size={'sm'}
+            className="bg-foreground text-background hover:text-foreground"
+            onClick={() => login()}
+            disabled={isLoading}
+          >
+            Sign In
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,9 +1,18 @@
 import { Outlet } from 'react-router';
 import Header from './Header';
+import LoadingScreen from './LoadingScreen';
+import { useAuth0 } from '@auth0/auth0-react';
+import { toast } from 'sonner';
 
 const MainLayout = () => {
+  const { isLoading, error } = useAuth0();
+
+  if (error)
+    toast.error(error?.message ?? 'Ocurrió un error al iniciar sesión');
+
   return (
     <div className="flex flex-col w-full h-dvh bg-background text-foreground">
+      {isLoading && <LoadingScreen />}
       <Header />
       <div className="p-4">
         <Outlet />
