81512d3525cade825fa1cf8ca68cc32e08a22911
devMini <edgar.danmm@gmail.com>
2026-05-04 13:06:03 -0600

    Add(map-overview): add constants file to store constants such as messages

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const CLICK_MAP_REPORT_WITHOUT_AUTH =
+  'You must be logged in to submit a report on a selected point inside the map.';
+
+export const CLICK_MAP_ROUTE_WITHOUT_AUTH =
+  'You must be logged in to get route directions.';
