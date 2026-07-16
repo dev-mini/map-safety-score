db6967b1c334fc5096e762fdee1ff2f4f8daa7c7
devMini <edgar.danmm@gmail.com>
2026-07-16 14:13:27 -0600

    Update(map-overview): update map overview layout spacing

--- src/pages/MapOverview.tsx
+++ src/pages/MapOverview.tsx
@@ -5,7 +5,7 @@ import MapOverviewBody from '@/features/Map/MapOverviewBody';
 const MapOverview = () => {
   return (
     <MapOverviewContextProvider>
-      <div className="flex flex-col h-full">
+      <div className="flex flex-col h-full gap-8">
         <MapOverviewHeader />
         <MapOverviewBody />
       </div>
